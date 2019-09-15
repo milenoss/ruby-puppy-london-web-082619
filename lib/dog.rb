97c9d25e2dc0fb10ff890bd6f75407a879cbdae2
Milen Khanal <milenkhanal@gmail.com>
2019-09-15 21:13:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,61 +1,41 @@
 
-class Dog 
-  @@all = [] 
-  def initialize(name)
-    
-   @@all << self
-  end
-  
-  def self.all 
-    @@all 
-  end
-  
-  def self.clear_all
+
+
+
+
+
+
+
+
+
+
+
+class Dog
     @@all = []
-  end
-  
-  def self.print_all 
-    @@all.each{|a| puts a.name } 
-  end
+    attr_accessor :name
+ 
+    def initialize(name)
+        @name = name
+        save
+    end
+ 
+    def self.all
+        @@all
+    end
+ 
+    def self.clear_all
+        @@all.clear
+    end
+ 
+    def self.print_all
+        @@all.each {|a|
+        puts a.name}
+    end
+ 
+    def save
+        @@all << self
+    end
+ 
+ 
+ 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-# class Dog
-#     @@all = []
-#     attr_accessor :name
- 
-#     def initialize(name)
-#         @name = name
-#         save
-#     end
- 
-#     def self.all
-#         @@all
-#     end
- 
-#     def self.clear_all
-#         @@all.clear
-#     end
- 
-#     def self.print_all
-#         @@all.each {|a|
-#         puts a.name}
-#     end
- 
-#     def save
-#         @@all << self
-#     end
- 
- 
- 
-# end
